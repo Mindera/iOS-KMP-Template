7b2d2b7415bfd6e4f49952435fbedc2cc949b17d
timea-v <timea.varga@mindera.com>
2024-04-17 17:11:24 +0300

    MKMPT-13: Fixes after merge in develop.

--- iOSKMP/iOSKMP/iOSKMPApp.swift
+++ iOSKMP/iOSKMP/iOSKMPApp.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import SwiftData
 import CurrencyExchangeKMP
 
 @main
@@ -14,23 +13,10 @@ struct iOSKMPApp: App {
     init() {
         IOSKoinHelperKt.doInitKoin()
     }
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
 
     var body: some Scene {
         WindowGroup {
             MenuTabView()
         }
-        .modelContainer(sharedModelContainer)
     }
 }
